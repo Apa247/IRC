e5cf796c90a9a75a58e52466aae6c117beb3e648
Apa247 <david.aparicio247@hotmail.com>
2025-03-22 11:47:18 +0100

    cambiado estructura socket

--- src/user_manager.cpp
+++ src/user_manager.cpp
@@ -6,7 +6,7 @@
 /*   By: jverdu-r <jverdu-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 16:01:15 by jverdu-r          #+#    #+#             */
-/*   Updated: 2025/03/19 18:16:38 by jverdu-r         ###   ########.fr       */
+/*   Updated: 2025/03/21 16:48:13 by jverdu-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,38 @@ UserManager::UserManager(std::map<int, std::string>& usernames) : usernames(user
 
 void UserManager::setUserName(int client_fd, const std::string& username)
 {
-	usernames[client_fd] = username;
+    usernames[client_fd] = username;
 }
 
 std::string UserManager::getUserName(int client_fd)
 {
-	if (usernames.find(client_fd) != usernames.end())
-	{
-		return usernames[client_fd];
-	}
-	return "";
+    if (usernames.find(client_fd) != usernames.end())
+    {
+        return usernames[client_fd];
+    }
+    return "";
 }
 
 std::string UserManager::getUserChannel(int client_fd) const {
-    if (user_channels.find(client_fd) != user_channels.end()) {
+    if (user_channels.find(client_fd) != user_channels.end())
+    {
         return user_channels.at(client_fd);
     }
-    return ""; // O algún valor predeterminado si el usuario no tiene un canal
+    return "";
 }
 
-void UserManager::setUserChannel(int client_fd, const std::string& channel){
+void UserManager::setUserChannel(int client_fd, const std::string& channel)
+{
     user_channels[client_fd] = channel;
     std::cout << "Usuario " << client_fd << " se ha unido al canal: " << channel << std::endl;
+}
+
+bool UserManager::userNameExists(const std::string& username)
+{
+    for (std::map<int, std::string>::const_iterator it = usernames.begin(); it != usernames.end(); ++it) {
+        if (it->second == username) {
+            return true;
+        }
+    }
+    return false;
 }
